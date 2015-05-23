0e70c61d6af2d85b2ec8e013139d20e79fdcea4a
xu.du <xu.du@socialcredits.cn>
2015-05-23 12:09:02 +0800

    weibobaoshi

--- weibo_baoshi.py
+++ weibo_baoshi.py
@@ -7,18 +7,16 @@ import datetime
 import re
 import os
 import time
-from PIL import Image
-
 reload(sys)
 sys.setdefaultencoding('utf-8')
 from weibo import Client
 
-APP_KEY = ''  # app key
-APP_SECRET = ''  # app secret
-CALLBACK_URL = ''  # callback url
-AUTH_URL = ''
-USERID = ''  # userid
-PASSWD = ''  # password
+APP_KEY = '41*********26'  # app key
+APP_SECRET = '4e***********520'  # app secret
+CALLBACK_URL = 'https://api.weibo.com/oauth2/default.html'  # callback url
+AUTH_URL = 'https://api.weibo.com/oauth2/authorize'
+USERID = '18******0'  # userid
+PASSWD = '*********'  # password
 
 
 def post_new(content, pic, location):
@@ -27,6 +25,8 @@ def post_new(content, pic, location):
 
 
 def gen_content(hour):
+    if hour == 0:
+        return u'新的一天已经到来~~~'
     if hour == 6:
         return u'早上好，该起床啦~~~'
     if hour == 8:
@@ -50,18 +50,6 @@ def gen_content(hour):
     return u'晚间整点报时~~~'
 
 
-def part_of_image(path, hour):
-    part_x = hour / 6
-    part_y = hour % 6
-    img = Image.open(path)
-    x, y = img.size
-    x, y = x/6, y/4
-    box = (part_y * x, part_x * y, (1 + part_y) * x, (1 + part_x) * y)
-    tmp_path = '/data/bing_bg/%s.jpg' % str(int(time.time()))
-    img.crop(box).save(tmp_path)
-    return tmp_path
-
-
 def get_today():
     """
     gen a string like '20150520'
@@ -74,27 +62,22 @@ def gen_location():
     # -180 - +180
     return random.randint(-90, 90), random.randint(-180, 180)
 
+base = '/data/images/mzt/'
+def get_img_path():
+    for x in os.listdir(base):
+        for y in os.listdir(base + x):
+            return base + x + '/' + y
+
 
 def post_hour_part():
     now_time = unicode(datetime.datetime.now())[:16]
     hour = map(int, re.findall(u'\\d+', now_time))[3]
-    status = u'现在是' + now_time + u'，' + gen_content(hour) + u'下面是今天bing首页壁纸的第%d/24块(将在今天最后一分钟发布完整的图片)' % (hour + 1)
-    part_img_path = part_of_image('/data/bing_bg/%s.jpg' % get_today(), hour)
-    post_new(status, open(part_img_path, 'rb'), gen_location())
-    os.remove(part_img_path)
+    status = u'现在是' + now_time + u'，' + gen_content(hour) + u'妹子图来了~~~'
+    img_path = get_img_path()
+    post_new(status, open(img_path, 'rb'), gen_location())
+    os.remove(img_path)
 
 
-def post_today_bing_bg():
-    status = u'今天即将结束，把今天分成24份发出去的bing首页壁纸完整发一遍吧，晚安~~~'
-    post_new(status, open('/data/bing_bg/%s.jpg' % get_today(), 'rb'), gen_location())
+if __name__ == '__main__': 
+    post_hour_part()
 
-
-if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print 'Argument wrong, need 2, got', len(sys.argv)
-    elif sys.argv[1].strip() == 'hour':
-        post_hour_part()
-    elif sys.argv[1].strip() == 'end':
-        post_today_bing_bg()
-    else:
-        print 'Unsupported argument :', sys.argv[1]
